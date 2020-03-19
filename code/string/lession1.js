88c86721779eacf28671258eefec2d2ae86844ee
cjshinning <784319417@qq.com>
2020-03-19 22:46:47 +0800

    2-3 反转单词代码演示

--- code/string/lession1.js
+++ code/string/lession1.js
@@ -8,8 +8,20 @@
 //     return result.join(' ');
 // }
 
+// export default (str) => {
+//     return str.split(' ').map(item => {
+//         return item.split('').reverse().join('')
+//     }).join(' ');
+// }
+
+// export default (str) => {
+//     return str.split(/\s/g).map(item => {
+//         return item.split('').reverse().join('')
+//     }).join(' ');
+// }
+
 export default (str) => {
-    return str.split(' ').map(item => {
+    return str.match(/[\w']+/g).map(item => {
         return item.split('').reverse().join('')
     }).join(' ');
 }
