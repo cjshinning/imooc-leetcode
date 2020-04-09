5dd1ad79feb8c65f9369c004301e46cdf5345c7b
cjshinning <784319417@qq.com>
2020-04-09 22:56:59 +0800

    12-3 堆排序-代码实操1

---
+++ code/heap/lession1.js
@@ -0,0 +1,52 @@
+class Heap{
+    constructor(data){
+        this.data = data;
+    }
+    sort(){
+        let iArr = this.data;
+        let n = iArr.length;
+        if(n < 1){
+            return iArr;
+        }else{
+            for(let i = Math.floor(n/2);i >= 0; i--){
+                Heap.maxHeapify(iArr, i, n);
+            }
+            for(let j=0;j<n;j++){
+                Heap.swap(iArr,0,n-1-j);
+                Heap.maxHeapify(iArr, 0, n-1-j-1);
+            }
+            return iArr;
+        }
+    }
+    // 交换两个元素的方法
+    static swap(arr,a,b){
+        if(a === b){
+            return '';
+        }
+        let c = arr[a];
+        arr[a] = arr[b];
+        arr[b] = c;
+    }
+    // 构建最大堆过程
+    static maxHeapify(Arr,i,size){
+        // 左节点索引
+        let l = i*2 + 1;
+        // 右节点索引
+        let r = i*2 + 2;
+        let largest = i;
+        // 父节点i和左节点l作比较取最大
+        if(l <= size && Arr[l] > Arr[largest]){
+            largest = l;
+        }
+        // 右节点和最大值比较
+        if(r < size && Arr[r] > Arr[largest]){
+            largest = r;
+        }
+        if(largest !== i){
+            Heap.swap(Arr, i, largest);
+            Heap.maxHeapify(Arr, largest, size);
+        }
+    }
+}
+
+export default Heap;
